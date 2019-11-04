d5b8329053bf76c1ebb447ce66b03865f52b01a9
Age <0800.grc@gmail.com>
2019-11-04 15:39:08 -0300

    Adding custom pt-br dict

--- business/spellcheck.business.js
+++ business/spellcheck.business.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const SpellChecker = require('simple-spellchecker');
 
 const cleanseWord = Symbol('cleanseWord');
+const getDictionary = Symbol('getDictionary');
 
 class SpellcheckBusiness {
   constructor(language) {
@@ -8,13 +10,7 @@ class SpellcheckBusiness {
   }
 
   async checkMistakes(corpus) {
-    const dictionary = await new Promise((resolve, reject) => {
-        SpellChecker.getDictionary(this.language, (err, dictionary) => {
-          if (err) return reject(err);
-
-          resolve(dictionary);
-        });
-      });
+    const dictionary = await this[getDictionary](); 
 
     const iterableCorpus = corpus.split(' ').map(w => this[cleanseWord](w));
 
@@ -30,7 +26,25 @@ class SpellcheckBusiness {
   }
 
   [cleanseWord](word) {
-    return word.replace(/[`~!@#$%^&*()_|+\-=?;:",.<>\{\}\[\]\\\/]/gi, '');// .replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ")
+    return word.replace(/[`~!@#$%^&*()_|+\=?;:",.<>\{\}\[\]\\\/]/gi, '');// .replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ")
+  }
+
+  async [getDictionary]() {
+    const appDir = path.dirname(require.main.filename);
+    const customDictLanguages = ['pt-br'];
+    let folderPath = null;
+
+    if (this.language && customDictLanguages.indexOf(this.language.toLowerCase()) > -1) {
+      folderPath = appDir + '/custom-dicts';
+    }
+
+    return new Promise((resolve, reject) => {
+      SpellChecker.getDictionary(this.language, folderPath, (err, dictionary) => {
+        if (err) return reject(err);
+
+        resolve(dictionary);
+      });
+    });
   }
 }
 
